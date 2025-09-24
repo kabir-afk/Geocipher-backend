b1b30d14b9e0a33dda7cdd09095502b4f416a26d
Kabir Singh <kabirsingh12b@gmail.com>
2025-09-25 03:08:34 +0530

    chore : post req for Score added

--- backend/api/views.py
+++ backend/api/views.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
 from .models import User , Coordinates
-from .serializers import UserSerializer , CoordinatesSerializer
+from .serializers import UserSerializer , CoordinatesSerializer , ScoreSerializer
 # Create your views here.
 class CoordinatesList(APIView):
 
@@ -11,3 +11,12 @@ class CoordinatesList(APIView):
         coords = Coordinates.objects.first()
         serializer = CoordinatesSerializer(coords)
         return Response(serializer.data)
+
+class Score(APIView):
+    def post(self,req):
+        data = req.data
+        serializer = ScoreSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        
