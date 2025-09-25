6032b59e605f9f3f14bae18845fa4c7d49bae41f
Kabir Singh <kabirsingh12b@gmail.com>
2025-09-25 21:18:51 +0530

    added urlspatterns for score

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.CoordinatesList.as_view()),
-    path('score', views.Score.as_view()),
+    path('score', views.ScoreList.as_view()),
 ]
