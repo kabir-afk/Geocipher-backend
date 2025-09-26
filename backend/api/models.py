e5c88f26d8aaf54716be4fb32aa03804084bba4f
Kabir Singh <kabirsingh12b@gmail.com>
2025-09-26 21:05:50 +0530

    update : Profiles model now uses in built User model

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,12 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 import json
 from pathlib import Path
 
 # Create your models here.
-class User(models.Model):
-    username = models.CharField(max_length=10)
-    email=models.EmailField(default="johdoe@exmaple.com")
-    password = models.CharField(max_length=20)
+class Profile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    bio = models.TextField()
 
     def __str__(self):
         return self.username
