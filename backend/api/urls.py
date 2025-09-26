880994c1847d9ba5da87185f829ccb15cd676ffc
Kabir Singh <kabirsingh12b@gmail.com>
2025-09-26 21:06:22 +0530

    registered Profile model

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,4 +3,6 @@ from . import views
 urlpatterns = [
     path('', views.CoordinatesList.as_view()),
     path('score', views.ScoreList.as_view()),
+    path('google/login/', views.GoogleLogin.as_view(), name='google_login'),
+    path('users/me/', views.UserMe.as_view(), name='user_detail')
 ]
