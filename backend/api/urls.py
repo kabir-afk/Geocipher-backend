dc21c1396130e1ea59ff05cba32aadafeb055cc6
Kabir Singh <kabirsingh12b@gmail.com>
2025-10-05 15:51:52 +0530

    added google login url

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,6 +3,5 @@ from . import views
 urlpatterns = [
     path('', views.CoordinatesList.as_view()),
     path('score', views.ScoreList.as_view()),
-    path('google/login/', views.GoogleLogin.as_view(), name='google_login'),
-    path('users/me/', views.UserMe.as_view(), name='user_detail')
+    path('auth/google', views.GoogleLogin.as_view(), name='google_login'),
 ]
