da2cd1e6427ab9630ce4c5433342807104df3d38
Kabir Singh <kabirsingh12b@gmail.com>
2025-10-05 15:53:03 +0530

    chore : google login class added

--- backend/api/views.py
+++ backend/api/views.py
@@ -4,22 +4,13 @@ from rest_framework.views import APIView
 from rest_framework import status
 from .models import User , Coordinates, Score
 from .serializers import UserSerializer , CoordinatesSerializer , ScoreSerializer
-import math
-# Create your views here.
+from .utils import score_exponential , get_id_token# Create your views here.
 class CoordinatesList(APIView):
 
     def get(self,req):
         coords = Coordinates.objects.first()
         serializer = CoordinatesSerializer(coords)
         return Response(serializer.data)
-
-def score_exponential(distance, max_score=5000, decay_rate=0.0005):
-    """
-    Score decreases exponentially with distance.
-    Closer guesses are rewarded much more heavily.
-    """
-    return round(max_score * math.exp(-decay_rate * distance))
-
 
 class ScoreList(APIView):
     def get(self,req):
@@ -37,3 +28,11 @@ class ScoreList(APIView):
             serializer.save()
             return Response(serializer.data,status=status.HTTP_201_CREATED)
         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    
+
+class GoogleLogin(APIView):
+    def post(self,req):
+        if 'code' in req.data.keys():
+            code = req.data['code']
+            id_token = get_id_token(code)
+        return Response("ok")
