5ba99f7bb3b4d6cd17b4f468e56d7c6522386454
Kabir Singh <kabirsingh12b@gmail.com>
2025-10-08 19:39:11 +0530

    added auth url

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,5 +3,6 @@ from . import views
 urlpatterns = [
     path('', views.CoordinatesList.as_view()),
     path('score', views.ScoreList.as_view()),
+    path('auth/', views.RegisterView.as_view()),
     path('auth/google', views.GoogleLogin.as_view(), name='google_login'),
 ]
