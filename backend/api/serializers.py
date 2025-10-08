ae3a61c2c928f23250de9ac7779f4d3278db5f4d
Kabir Singh <kabirsingh12b@gmail.com>
2025-10-08 19:15:24 +0530

    update : added UserSerializer

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,11 +1,15 @@
 from rest_framework import serializers
-from .models import User
+from django.contrib.auth.models import User
 from .models import Coordinates, Score
 
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
     class Meta :
         model = User
-        fields = "__all__"
+        fields = ["username,email,password"]
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
 
 class CoordinatesSerializer(serializers.ModelSerializer):
     class Meta:
