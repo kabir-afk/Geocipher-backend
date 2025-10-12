c78f47f2d4302a4366802a428b5961002aaaebce
Kabir Singh <kabirsingh12b@gmail.com>
2025-10-12 15:34:34 +0530

    chore : added urlpattern for protected view

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('score', views.ScoreList.as_view()),
     path('auth/', views.RegisterView.as_view()),
     path('auth/google', views.GoogleLogin.as_view(), name='google_login'),
+    path('protected-view/', views.ProtectedView.as_view()),
 ]
