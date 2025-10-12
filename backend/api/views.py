2ab40cbb10ba3632b95a2642ed3703b539272453
Kabir Singh <kabirsingh12b@gmail.com>
2025-10-12 15:33:47 +0530

    [WIP] feat : dummy protected view

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from rest_framework.response import Response
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny,IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework import status , generics
 from .models import Coordinates, Score
@@ -43,3 +43,11 @@ class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [AllowAny]
+
+class ProtectedView(APIView):
+    permission_classes=[IsAuthenticated]
+    def get(self,req):
+        data={
+            'status':"authenticated user's protected view" 
+        }
+        return Response(data)
