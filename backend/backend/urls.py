dbe6ab984611f8e29d34e7b04a7ac6bd07cb7ba9
Kabir Singh <kabirsingh12b@gmail.com>
2025-10-12 15:32:59 +0530

    chore : added urlpatterns for token views

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -17,15 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from . import views
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home),
     path('api/v1/', include('api.urls')),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/', views.CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', views.CookieTokenRefreshView.as_view(), name='token_refresh'),
 ]
