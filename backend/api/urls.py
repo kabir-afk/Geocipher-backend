43f4ac3659106490c176e00ac546983368aea90a
Kabir Singh <kabirsingh12b@gmail.com>
2025-10-19 18:49:52 +0530

    chore : added url pattern for game-id

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.CoordinatesList.as_view()),
+    path('game-id', views.GameView.as_view()),
     path('score', views.ScoreList.as_view()),
     path('auth/', views.RegisterView.as_view()),
     path('auth/google', views.GoogleLogin.as_view(), name='google_login'),
