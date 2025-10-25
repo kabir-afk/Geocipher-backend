35d52082d9b5735d211d2f2809572666564cdbf5
Kabir Singh <kabirsingh12b@gmail.com>
2025-10-25 21:46:06 +0530

    feat : added ASGI config

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -8,9 +8,22 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
 
 import os
-
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from django.urls import path
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
-application = get_asgi_application()
+django_asgi_app = get_asgi_application()
+
+from api.consumer import LobbyConsumer
+
+application = ProtocolTypeRouter({
+	"http": django_asgi_app,
+	"websocket": AuthMiddlewareStack(
+		URLRouter([
+			path("ws/", LobbyConsumer.as_asgi()),
+		])
+	),
+})
