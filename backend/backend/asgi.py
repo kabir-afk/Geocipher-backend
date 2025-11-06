31055e511754bfbfb9679d46b3696ef473f56859
Kabir Singh <kabirsingh12b@gmail.com>
2025-11-06 22:27:22 +0530

    update : settings module path

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for backend project.
+ASGI config for backend.backend project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -12,7 +12,7 @@ from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.urls import path
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.backend.settings')
 
 django_asgi_app = get_asgi_application()
 
