5a62c8a90169a1559413f48aaed6ed5421340667
Kabir Singh <kabirsingh12b@gmail.com>
2025-11-11 20:19:52 +0530

    update : allowed host

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -34,7 +34,7 @@ if ENVIRONMENT == 'development':
 else:
     DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '*']
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
 
 
 # Application definition
